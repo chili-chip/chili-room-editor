14cae486d4b10e3ebddefda96ed0fb425aa3937b
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2025-10-05 14:45:37 +0200

    add tile validation

--- src/components/Palette.svelte
+++ src/components/Palette.svelte
@@ -24,11 +24,40 @@
     ctx.drawImage(src, sx, sy, size, size, 0, 0, $paletteTileSize, $paletteTileSize);
     return off.toDataURL();
   }
+
+  $: validTiles = $tilesetBitmap ? (() => {
+    const total = $tilesPerRow * $tileRows;
+    const hasPixels = new Set();
+    for (let i = 0; i < total; i++) {
+      const tpr = $tilesPerRow;
+      const size = FIXED_TILE_SIZE;
+      const sx = (i % tpr) * size;
+      const sy = Math.floor(i / tpr) * size;
+      const src = document.createElement('canvas');
+      src.width = $tilesetBitmap.width; src.height = $tilesetBitmap.height;
+      const sctx = src.getContext('2d');
+      sctx.drawImage($tilesetBitmap, 0, 0);
+      const imageData = sctx.getImageData(sx, sy, size, size);
+      const data = imageData.data;
+      let tileHasPixels = false;
+      for (let j = 3; j < data.length; j += 4) {
+        if (data[j] > 0) {
+          tileHasPixels = true;
+          break;
+        }
+      }
+      if (tileHasPixels) hasPixels.add(i);
+    }
+    if (hasPixels.size === 0) return [];
+    const min = Math.min(...hasPixels);
+    const max = Math.max(...hasPixels);
+    return Array.from({length: max - min + 1}, (_, i) => min + i);
+  })() : [];
 </script>
 
 <div id="palette" style={`--tile: ${$paletteTileSize}px`}>
   {#if $tilesetBitmap}
-    {#each Array($tilesPerRow * $tileRows) as _, i}
+    {#each validTiles as i}
       <button type="button" class:selected={$currentBrush === i} style={`background-image:url(${tileDataURL(i)}); background-size: ${$paletteTileSize}px ${$paletteTileSize}px;`} on:click={() => pick(i)}></button>
     {/each}
   {:else}
